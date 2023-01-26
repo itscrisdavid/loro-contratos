f5aee6e558e6e2096a8c27d2c45fb541f69c5ec0
Cristian Álvarez <46637796+itscrisdavid@users.noreply.github.com>
2023-01-25 20:44:21 -0500

    Update index.php

--- sistema/index.php
+++ sistema/index.php
@@ -18,7 +18,7 @@ $usuario = $_SESSION['iduser'];
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>INICIO</title>
+  <title>Panel - Loro Musical</title>
 
   <?php include "includes/scriptsUp.php" ?>
 </head>
